f6784acbaa2a1e70f143e06c2b1026a103cb26d2
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-03-21 09:14:14 -0500

    Operaciones

--- helloWorld/main.cpp
+++ helloWorld/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
-using namespace std;
+int main(){
+    int x=5;
+    int y=6;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+    std:: cout <<"la suma es: "<<x+y<< std ::endl;
+    std:: cout <<"la resta es: "<<x-y<< std ::endl;
+    std:: cout <<"el producto es: "<<x*y<< std ::endl;
+    std:: cout <<"el cociente es: "<<(float)x/y<< std ::endl;
     return 0;
 }
