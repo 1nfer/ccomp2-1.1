7a9ca0221ed5c2c13027eec250d2652479c9d293
1nfer <37373380+1nfer@users.noreply.github.com>
2018-04-04 09:15:52 -0500

    Update problema4.cpp

--- TrabajoPractico_1/problema4.cpp
+++ TrabajoPractico_1/problema4.cpp
@@ -5,36 +5,15 @@ using namespace std;
 int main()
 {
     int a,b;
-    cin>>a;
+    cin>>a>>b;
 
-    if(a%2){
-        cout<<"a no es multiplo de 2"<<endl;
-    }
-    else{
-        cout<<"a es multiplo de 2"<<endl;
-    }
+    (a%2)?cout<<a<<" no multiplo de 2"<<endl:cout<<a<<" multiplo de 2"<<endl;
 
-    cin>>b;
+    (b%2)?cout<<b<<" no multiplo de 2"<<endl:cout<<b<<" multiplo de 2"<<endl;
 
-    if(b%2){
-        cout<<"a no es multiplo de 2"<<endl;
-    }
-    else{
-        cout<<"b es multiplo de 2"<<endl;
-    }
+    (a%b)?cout<<a<<" no multiplo de "<<b<<endl:cout<<a<<" multiplo de "<<b<<endl;
 
-    if(a%b){
-        cout<<"a no es multiplo de b"<<endl;
-    }
-    else{
-        cout<<"a es multiplo de b"<<endl;
-    }
+    (b==a*a)?cout<<a<<" al cuadrado es "<<b<<endl:cout<<a<<" al cuadrado no es "<<b<<endl;
 
-    if(b==a*a){
-        cout<<"a al cuadro es b"<<endl;
-    }
-    else{
-        cout<<"a al cuadrado no es b"<<endl;
-    }
     return 0;
 }
