c1985cd50f504883fe98e2d7e31fe768b220a178
1nfer <37373380+1nfer@users.noreply.github.com>
2018-04-18 10:03:03 -0500

    Add files via upload

---
+++ python/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+int sumaIte(const int a[], int tam){
+    int sum=0;
+    for(int i=0;i<tam;sum+=a[i++]);
+    return sum;
+}
+
+int sumaRec(const int a[], int tam){
+    if(tam==1){
+        return a[0];
+    }
+    tam--;
+    return a[tam]+sumaRec(a,tam);
+}
+
+void invertir(int a[],int tam){
+    int izq=0;
+    int der=tam-1;
+    while(izq<=der){
+        int aux=a[izq];
+        a[izq]=a[der];
+        a[der]=aux;
+        izq++;
+        der--;
+    }
+}
+
+int size(char s[]){
+    int tam=0;
+    for(;s[tam]!='\0';tam++);
+    return tam;
+}
+
+int cpy(char s[], char t[]){
+    for(int i=0;t[i]!='\0';i++){
+        s[i]=t[i];
+    }
+}
+int add(char s[], char t[]){
+    int tam=0;
+    while(s[tam]!='\0'){
+        tam++;
+    }
+    for(int i=0;t[i]!='\0';i++){
+        s[tam]=t[i];
+        tam++;
+    }
+}
+
+int main(){
+    int a[]={1,5,6,4};
+    cout<<sumaIte(a,4)<<endl;
+    cout<<sumaRec(a,4)<<endl;
+    invertir(a,4);
+    char s[]="hay ocho";
+    char t[]="no hay";
+    cout<<size(s)<<endl;
+    cpy(s,t);
+    cout<<s<<endl;
+    add(s,t);
+    cout<<s;
+
+    return 0;
+}
